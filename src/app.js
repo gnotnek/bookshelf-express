0f4d040527f51191788402ab7c1b0455126e24e8
Dhista Ardiansyah <56432757+gnotnek@users.noreply.github.com>
2023-11-05 15:10:58 +0700

    Add PORT, DB_URL, and HOST to .env.example and install dotenv package.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const routes = require('./routes')
 const moongose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
-const port = 9000
-const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
+const port = process.env.PORT || 3000
+const host = process.env.HOST || 'localhost'
 
 const dbURL = 'mongodb://127.0.0.1:27017/bookshelf'
 moongose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
