410025e825039575d27f9300713d85148a494e54
Dhista Ardiansyah <56432757+gnotnek@users.noreply.github.com>
2023-11-06 08:53:57 +0700

    Update MongoDB service and port number

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const routes = require('./routes')
 const moongose = require('mongoose')
+
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = 9000
 const host = process.env.HOST || 'localhost'
 
 const dbURL = 'mongodb://127.0.0.1:27017/bookshelf'
